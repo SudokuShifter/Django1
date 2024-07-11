09d92aab4e08590e86618080ecf5df9ababeb548
mr.Robot <zanid227@yandex.ru>
2024-07-11 21:33:00 +0300

    add debug-toolbar some change in project settings

--- myhomeproject/myhomeproject/urls.py
+++ myhomeproject/myhomeproject/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firstapp.urls')),
     path('shop/', include('secondapp.urls')),
+    path('__debug__/', include('debug_toolbar.urls'))
 ]
 
 if settings.DEBUG:
